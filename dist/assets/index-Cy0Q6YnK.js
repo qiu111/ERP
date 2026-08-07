965ec64e1e1e7e9a95c90b1cb559518f4da0e58f
Alice <33218591+qiu111@users.noreply.github.com>
2026-08-07 10:44:55 +0800

    Delete dist directory

--- dist/assets/index-Cy0Q6YnK.js
+++
@@ -1,1 +0,0 @@
-import{J as s}from"./index-D15sTKCi.js";const l=s("menu",{state:()=>({collapse:!1,menuList:[]}),getters:{getCollapse:e=>e.collapse,getMenuList:e=>e.menuList},actions:{setCollapse(e){this.collapse=e},setMenuList(e){this.menuList=e}}});export{l as default};
